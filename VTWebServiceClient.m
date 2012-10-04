dea36f50d2297efa269b33e0090e224900c455e0
Adam Heinz <amh@metricwise.net>
2012-10-04 14:31:19 -0400

    Adding software licenses to comments. Removing internal methods from interface.

--- VTWebServiceClient.m
+++ VTWebServiceClient.m
@@ -1,10 +1,12 @@
-//
-//  WebServiceClient.m
-//  Window&DoorMeasurementNew
-//
-//  Created by Adam Heinz on 9/25/12.
-//
-//
+/*
+ Copyright (c) 2012 MetricWise, Inc
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #import <CommonCrypto/CommonDigest.h>
 
@@ -116,11 +118,6 @@
     [urlRequest setValue:[NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary] forHTTPHeaderField:@"content-type"];
     NSData *responseData = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:nil error:nil];
     return [self parseResponse:responseData];
-}
-
-- (NSDictionary *)doQuery:(NSString *)query
-{
-    return nil;
 }
 
 - (NSString *) md5:(NSString *)str {
